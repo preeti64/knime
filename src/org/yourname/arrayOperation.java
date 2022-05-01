7565a1804ca3443fb03f4ce1e654066df806c851
preeti64 <58847237+preeti64@users.noreply.github.com>
2022-05-01 11:21:06 +0200

    Update arrayOperation.java

--- src/org/yourname/arrayOperation.java
+++ src/org/yourname/arrayOperation.java
@@ -1,16 +1,45 @@
 import java.util.Arrays;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner; 
+import java.io.PrintWriter;
 
 public class MyClass {
     public static void main(String args[]) {
      String[] claArray = args;
      System.out.println(Arrays.toString(claArray));
-     String output ="";
-     int num =123;
-     String text = "rohit";
+     ArrayList<String> lines = new ArrayList<>();
+     
+     int files = 0; // showing the numnber of Command Line Arguments that are file
+		String inFile = " ";
+	    String outFile = " ";
+	   
+	    for(int i = 0; i < args.length; i++){
+		         String arg = args[i];
+		          files++;
+		             
+		             if(files == 2){
+		                 inFile = arg;
+		                 
+		             }
+		             
+		             else if(files == 10){
+		                 outFile = arg;
+		             }
+	    }
+	    
+		
      String inputType = args[3];
 
      String operations = args[5];
- 		if(inputType.equals("int") ) {
+      File file = new File(inFile);
+      try{
+          
+           Scanner in = new Scanner(file);
+		         while(in.hasNextLine()){
+		             String line = in.nextLine();
+		             if(inputType.equals("int") ) {
  		    
  		      if(operations.contains("rev")) {
  		          
@@ -29,6 +58,9 @@ public class MyClass {
  		    
  		}
  		
+		             
+      
+ 		
  			if(inputType.equals("String") ) {
  		    
  		      if(operations.contains("rev")) {
@@ -45,9 +77,32 @@ public class MyClass {
  		      
  		    
  		}
+		         }
  	
 
     }
+    
+     catch(IOException e)
+                 {
+                    e.printStackTrace();
+                    System.exit(0);
+                 }
+                 
+                  try{
+		         PrintWriter out = new PrintWriter(outFile);
+		         lines.forEach((line)->
+		         {
+		             out.println(line);
+		         });
+		         
+		     }
+		     
+		      catch(IOException e)
+                 {
+                    e.printStackTrace();
+                    System.exit(0);
+                 }
+    }             
     
      static int reverseNumber(int number) {
          int reverse=0;
